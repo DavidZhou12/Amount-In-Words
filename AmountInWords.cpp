6cbf7c323e1ae690189f9ae68c17306c29d87f84
David Zhou <DavidZhouToday@gmail.com>
2018-03-05 01:45:07 -0500

    Update AmountInWords.cpp

--- AmountInWords.cpp
+++ AmountInWords.cpp
@@ -7,17 +7,38 @@
 #include <string>
 #include <vector>
 
-std::string amountInWords(const std::string& s) {
-	int len = 0;
+std::string amountInWords(std::string s) {
+	int len = 0, n = 0;
 	std::string res;
 	std::vector<std::string> twos{ "", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
 									"ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" },
 		tens{ "", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" },
-		tens_power{ "hundred", "thousand", "million", "billion", "trillion", "quadrillion", "quintillion", "sextillion", "septillion" };
+		tens_power{ "", "thousand", "million", "billion", "trillion", "quadrillion", "quintillion", "sextillion", "septillion" };
 
+	// TO-DO: Check if the characters are a digit
 	for (std::string::reverse_iterator rit = s.rbegin(); rit != s.rend(); ++rit) {
-		
+		if (len++ > 0) res = ' ' + res;
+		if ((len - 1) % 3 == 0) {	// First Digit
+			n += *rit - '0';
+			res = tens_power[len / 3] + res;
+		}
+		else if (len % 3 == 0) {	// Third Digit
+			int tmp = *rit - '0';
+			if (tmp != 0)
+				res = twos[tmp] + " hundred" + res;
+		}
+		else {						// Second Digit
+			n += (*rit - '0') * 10;
+			if (n < 20)
+				res = twos[n] + res;
+			else
+				res = tens[n / 10] + '-' + twos[n % 10] + res;
+			n = 0;
+		}
 	}
+
+	if (n != 0) res = twos[n] + ' ' + res;
+	res[0] = toupper(res[0]);
 
 	return res;
 }
@@ -26,7 +47,7 @@ int main() {
 	std::string s;
 	std::cout << "Please enter the amount in decimal: ";
 	std::cin >> s;
-	std::cout << amountInWords(s);
+	std::cout << amountInWords(s) << std::endl;
 
     return 0;
 }
